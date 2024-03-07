ca40850661cfa5990188664a57698c6826f2d6a9
Artem Demchenko <artdem243@gmail.com>
2024-03-08 02:46:28 +0300

    Fix minor bugs

--- work1/main.cpp
+++ work1/main.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <functional>
 #include <cmath>
-#include <chrono>
 #include <iomanip>
 
 class NetSolver {
@@ -11,7 +10,7 @@ public:
     NetSolver(int n, int bsize, const std::function<double(double, double)> &g_fun_,
               const std::function<double(double, double)> &f_fun_) : actual_fun(g_fun_), N(n), H(1.0 / (n + 1)),
                                                                      BSIZE(bsize),
-                                                                     NB(n / bsize + (n % bsize == 0)) {
+                                                                     NB(n / bsize + (n % bsize != 0)) {
         u_arr.resize(N + 2, std::vector<double>(N + 2, 0));
         f_arr.resize(N + 2, std::vector<double>(N + 2, 0));
         for (int i = 0; i <= N + 1; i++) {
@@ -28,9 +27,11 @@ public:
     }
 
     void calculate_approximation() {
-        double dmax = 0;
         std::vector<double> dm(NB, 0);
+        double dmax;
         do {
+            dmax = 0;
+            std::fill(dm.begin(), dm.end(), 0);
             for (int nx = 0; nx < NB; nx++) {
 #pragma omp parallel for shared(nx, dm) default(none)
                 for (int i = 0; i <= nx; i++) {
@@ -41,7 +42,7 @@ public:
             }
             for (int nx = NB - 2; nx >= 0; nx--) {
 #pragma omp parallel for shared(nx, dm) default(none)
-                for (int i = 1; i <= nx; i++) {
+                for (int i = NB - nx - 1; i < NB; i++) {
                     int j = 2 * (NB - 1) - nx - i;
                     double d = block_processing(i, j);
                     dm[i] = std::max(dm[i], d);
@@ -69,9 +70,9 @@ private:
         for (int i = 1 + block_i * BSIZE; i <= std::min((block_i + 1) * BSIZE, N); i++) {
             for (int j = 1 + block_j * BSIZE; j <= std::min((block_j + 1) * BSIZE, N); j++) {
                 double temp = u_arr[i][j];
-                u_arr[i][j] = 0.25 * (u_arr[i - 1][j] + u_arr[i + 1][j] +
-                                      u_arr[i][j - 1] + u_arr[i][j + 1] - H * H * f_arr[i][j]);
-                dmax = std::max(dmax, temp - u_arr[i][j]);
+                u_arr[i][j] = 0.25 * fabs(u_arr[i - 1][j] + u_arr[i + 1][j] +
+                                          u_arr[i][j - 1] + u_arr[i][j + 1] - H * H * f_arr[i][j]);
+                dmax = std::max(dmax, fabs(temp - u_arr[i][j]));
             }
         }
         return dmax;
@@ -83,14 +84,14 @@ private:
     const std::function<double(double, double)> actual_fun;
 };
 
-#define TREADS_NUM 4
+#define THREADS_NUM 4
 
 int main() {
-    omp_set_num_threads(TREADS_NUM);
+    omp_set_num_threads(THREADS_NUM);
 
     auto actual_fun = [](double x, double y) { return 2 * pow(x, 5) + 3 * pow(y, 4); };
     auto actual_fun_d = [](double x, double y) { return 40 * pow(x, 3) + 36 * pow(y, 2); };
-    NetSolver net(10000, 32, actual_fun, actual_fun_d);
+    NetSolver net(3000, 16, actual_fun, actual_fun_d);
 
 
     auto start_time = omp_get_wtime();
