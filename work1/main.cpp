87786b39973150095baa1b578da250afeb621145
Artem Demchenko <artdem243@gmail.com>
2024-03-10 13:58:28 +0300

    Add max error and multiple launch

--- work1/main.cpp
+++ work1/main.cpp
@@ -57,6 +57,16 @@ public:
         } while (dmax > EPS);
     }
 
+    double calculate_max_error() {
+        double max_error = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                max_error = std::max(max_error, std::abs(u_arr[i][j] - expected_fun(i * h, j * h)));
+            }
+        }
+        return max_error;
+    }
+
     double calculate_mean_absolute_error() {
         double sum_error = 0;
         for (int i = 1; i <= N; i++) {
@@ -81,7 +91,7 @@ public:
                 }
             }
         }
-        return sum_error / (N * N -  zero_count);
+        return sum_error / (N * N - zero_count);
     }
 
 private:
@@ -109,17 +119,22 @@ private:
 int main() {
     omp_set_num_threads(THREADS_NUM);
 
-    auto fun = [](double x, double y) { return x * x + y; };
-    auto fun_d = [](double x, double y) { return 2; };
-    NetSolver net(500, 16, fun, fun_d);
+    auto test_fun = [](double x, double y) { return x + y; };
+    auto test_fun_d = [](double x, double y) { return 0; };
+    std::vector<int> test_net_sizes{10, 50, 100, 500, 1000, 5000};
 
+    for (auto it: test_net_sizes) {
+        NetSolver net(it, 16, test_fun, test_fun_d);
 
-    auto start_time = omp_get_wtime();
-    net.calculate_approximation();
-    auto end_time = omp_get_wtime();
+        auto start_time = omp_get_wtime();
+        net.calculate_approximation();
+        auto end_time = omp_get_wtime();
 
-    std::cout << std::fixed << std::setprecision(3);
-    std::cout << "WORK TIME: " << end_time - start_time << '\n';
-    std::cout << "MEAN ABSOLUTE ERROR: " << net.calculate_mean_absolute_error() << '\n';
-    std::cout << "APPROXIMATE ERROR: " << net.calculate_approximate_error();
+        std::cout << std::fixed << std::setprecision(6);
+        std::cout << "-------| Net size = " << it << " |-------\n";
+        std::cout << "WORK TIME: " << end_time - start_time << '\n';
+        std::cout << "MEAN ABSOLUTE ERROR: " << net.calculate_mean_absolute_error() << '\n';
+        std::cout << "MAX ERROR: " << net.calculate_max_error() << '\n';
+        std::cout << "APPROXIMATE ERROR: " << net.calculate_approximate_error() << '\n';
+    }
 }
