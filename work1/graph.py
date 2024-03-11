698f37f9d063c87f96b134acd1e1c7ae9ec5e436
Artem Demchenko <artdem243@gmail.com>
2024-03-11 03:01:40 +0300

    Clean up and add random init

--- work1/graph.py
+++ work1/graph.py
@@ -1,19 +1,32 @@
 import matplotlib.pyplot as plt
 
-data = []
-with open("eps_results.txt") as f:
-    for line in f:
-        data.append([float(x) for x in line.split()])
 
-size = len(data)
-x = [0 for i in range(size)]
-t = [0 for i in range(size)]
-e = [0 for i in range(size)]
-for i in range(size):
-    x[i], t[i], e[i] = data[i][0], data[i][1], data[i][2]
+def get_data(j):
+    data = []
+    with open(f"results{j}.txt") as f:
+        for line in f:
+            data.append([float(x) for x in line.split()])
+    size = len(data)
+    x = [data[i][0] for i in range(size)]
+    y = [data[i][1] for i in range(size)]
+    return x, y
 
-plt.title("Зависимость относительной погрешности от значения eps")
-plt.xlabel("eps")
-plt.ylabel("err")
-plt.plot(x, e)
+
+num = 3 # change
+all_data = [get_data(i) for i in range(num)]
+all_x = [all_data[i][0] for i in range(num)]
+all_y = [all_data[i][1] for i in range(num)]
+for i in range(num):
+    plt.scatter(all_x[i], all_y[i], s=2)
+
+plt.xlabel('Размер сетки (N)') # change
+plt.ylabel('Время, сек') # change
+plt.plot(all_x[0], all_y[0], linewidth=1, label='нули')
+plt.plot(all_x[1], all_y[1], linewidth=1, label='среднее значение')
+plt.plot(all_x[2], all_y[2], linewidth=1, label='случайные значения')
+
+plt.title("Сравнение производительности при разных\nначальных приближениях "
+          "для функции 1/sin(x+y+0.1), THREADS_NUM=4")
+plt.legend()
+
 plt.show()
--- work1/main.cpp
+++ work1/main.cpp
@@ -4,8 +4,7 @@
 #include <functional>
 #include <cmath>
 #include <iomanip>
-#include <csignal>
-#include <fcntl.h>
+#include <random>
 
 class NetSolver {
 public:
@@ -15,18 +14,28 @@ public:
         u_arr.resize(N + 2, std::vector<double>(N + 2, 0));
         f_arr.resize(N + 2, std::vector<double>(N + 2, 0));
         double mean = 0;
+        double min_val = 1e9;
+        double max_val = -1e9;
         for (int i = 0; i <= N + 1; i++) {
             u_arr[i][0] = g_fun_(i * h, 0);
             u_arr[i][N + 1] = g_fun_(i * h, (N + 1) * h);
             u_arr[0][i] = g_fun_(0, i * h);
             u_arr[N + 1][i] = g_fun_((N + 1) * h, i * h);
             mean += (u_arr[i][0] + u_arr[i][N + 1] + u_arr[0][i] + u_arr[N + 1][i]);
+            min_val = std::min(min_val, std::min(std::min(u_arr[i][0], u_arr[i][N + 1]),
+                                                 std::min(u_arr[0][i], u_arr[N + 1][i])));
+            max_val = std::max(max_val, std::max(std::max(u_arr[i][0], u_arr[i][N + 1]),
+                                                 std::max(u_arr[0][i], u_arr[N + 1][i])));
         }
         mean = (mean - u_arr[0][0] - u_arr[0][N + 1] - u_arr[N + 1][0] - u_arr[N + 1][N + 1]) / ((N + 1) * 4);
+
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_real_distribution<double> dist(min_val, max_val);
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
                 f_arr[i][j] = f_fun_(i * h, j * h);
-//                u_arr[i][j] = mean;
+                u_arr[i][j] = dist(mt); // change init value
             }
         }
     }
@@ -120,45 +129,53 @@ private:
 //    return 0.5 * x * x + y * y - y - 0.5 * x * y + 0.25;
 //};
 //auto test_fun_d = [](double x , double y) { return 3; };
+
 //auto test_fun = [](double x, double y) { return pow(x, 6) + pow(y, 6); };
 //auto test_fun_d = [](double x, double y) { return 30 * pow(x, 4) + 30 * pow(y, 4); };
+
 //auto test_fun = [](double x, double y) { return sin(x) + sin(y); };
 //auto test_fun_d = [](double x, double y) { return -sin(x) - sin(y); };
+
+//auto test_fun = [](double x, double y) { return 1 / sin(x + y + 0.1); };
+//auto test_fun_d = [](double x, double y) { return (1 + pow(cos(x + y + 0.1), 2)) / pow(sin(x + y + 0.1), 3); };
+
 #define THREADS_NUM 4
 
 void test_different_net_sizes() {
     omp_set_num_threads(THREADS_NUM);
-    auto test_fun = [](double x, double y) {
-        return 0.5 * x * x + y * y - y - 0.5 * x * y + 0.25;
-    };
-    auto test_fun_d = [](double x, double y) { return 3; };
-    std::vector<int> test_net_sizes{10, 50, 100, 500, 1000, 5000};
+    FILE *fp = freopen("results2.txt", "w", stdout); // change
+    auto test_fun = [](double x, double y) { return 1 / sin(x + y + 0.1); };
+    auto test_fun_d = [](double x, double y) { return 2 * (1 + pow(cos(x + y + 0.1), 2)) / pow(sin(x + y + 0.1), 3); };
+    std::vector<int> test_net_sizes{10, 20, 50, 100, 200, 300, 400, 500, 600, 800, 1000};
+//    for (int i = 500; i <= 5000; i += 500) {
+//        test_net_sizes.push_back(i);
+//    }
     for (auto it: test_net_sizes) {
-        NetSolver net(it, 16, 1e-2, test_fun, test_fun_d);
+        NetSolver net(it, 32, 1e-3, test_fun, test_fun_d);
 
         auto start_time = omp_get_wtime();
         net.calculate_approximation();
         auto end_time = omp_get_wtime();
 
-        std::cout << std::fixed << std::setprecision(6);
-        std::cout << "-------| Net size = " << it << " |-------\n";
-        std::cout << "WORK TIME: " << end_time - start_time << '\n';
-        std::cout << "APPROXIMATE ERROR: " << net.calculate_approximate_error() << '\n';
+        std::cout << std::fixed << std::setprecision(6) << it << ' ' << (end_time - start_time) << '\n';
     }
+    fclose(fp);
+}
+
+void test_different_epsilons() {
+    omp_set_num_threads(THREADS_NUM);
+    FILE *fp = freopen("results.txt", "w", stdout);
+    auto test_fun = [](double x, double y) { return 1 / sin(x + y + 0.1); };
+    auto test_fun_d = [](double x, double y) { return 2 * (1 + pow(cos(x + y + 0.1), 2)) / pow(sin(x + y + 0.1), 3); };
+    double step = 1e-5;
+    for (double eps = step; eps <= 0.01; eps += step) {
+        NetSolver net(100, 32, eps, test_fun, test_fun_d);
+        net.calculate_approximation();
+        std::cout << std::fixed << std::setprecision(6) << eps << ' ' << net.calculate_approximate_error() << '\n';
+    }
+    fclose(fp);
 }
 
 int main() {
-    omp_set_num_threads(THREADS_NUM);
-    auto test_fun = [](double x, double y) { return sin(x) + sin(y); };
-    auto test_fun_d = [](double x, double y) { return -sin(x) - sin(y); };
-    freopen("eps_results.txt", "w", stdout);
-    double step = 1e-5;
-    for (double eps = step; eps <= 0.1; eps += step) {
-        NetSolver net(100, 16, eps, test_fun, test_fun_d);
-        auto start_time = omp_get_wtime();
-        net.calculate_approximation();
-        auto end_time = omp_get_wtime();
-        std::cout << std::fixed << std::setprecision(6) << eps << ' ' << (end_time - start_time) << ' '
-                  << net.calculate_approximate_error() << '\n';
-    }
+    test_different_epsilons();
 }
